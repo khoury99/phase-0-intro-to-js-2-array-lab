c7d67009243d70e3918f28d5901ef50f6b544587
Elijah Green <elijah.khoury99@gmail.com>
2022-11-28 14:17:15 -0800

    commit tree 74b81c3f04371c98fa39c7cb1d8a333481ed3788

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+let cats=["Milo", "Otis", "Garfield"]
+// cats.push("Milo")
+// console.log(cats)
+function destructivelyAppendCat(cat){
+    cats.push(cat)
+    return cats
+}
+
+  function destructivelyPrependCat(cat) {
+    return cats.unshift(cat)
+  }
+
+  function destructivelyRemoveLastCat() {
+    return cats.pop()
+  }
+
+  function destructivelyRemoveFirstCat() {
+    return cats.shift()
+  }
+ 
+  function appendCat(name) {
+  const CopyOfCats= [...cats]
+  CopyOfCats.push(name)
+  return CopyOfCats
+}
+function prependCat(name) {
+    const CopyOfCats=  [...cats]
+    CopyOfCats.unshift(name)
+    return CopyOfCats
+}
+function removeLastCat(name) {
+    const CopyOfCats= [...cats]
+    CopyOfCats.pop(name)
+    return CopyOfCats
+}
+function removeFirstCat(name) {
+    const CopyOfCats= [...cats]
+    CopyOfCats.shift(name)
+    return CopyOfCats
+}
